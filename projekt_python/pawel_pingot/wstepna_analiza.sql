98ebe05a8c9dcfbc24e91da8caca5595da7b875a
Pawel <pawelpingott@gmail.com>
2020-01-07 14:22:54 +0100

    Dopełnienie analizy sql

--- projekt_python/pawel_pingot/wstepna_analiza.sql
+++ projekt_python/pawel_pingot/wstepna_analiza.sql
@@ -19,7 +19,7 @@ from (select distinct (case
       from "WA_Fn-UseC_-HR-Employee-Attrition"
       group by 1, 2
       order by 1) pod;
--- wpływ businesstravel i monthlyincome na attrition
+-- businesstravel->monthlyincome->attrition
 select *, round(liczba_pracowników / sum(liczba_pracowników) over (partition by businesstravel, zarobki), 2) as procent
 from (select businesstravel,
              (case
@@ -40,13 +40,13 @@ from (select businesstravel,
       group by 1, 2, 4
       order by zarobki, businesstravel) pod
 order by zarobki, pod.businesstravel;
--- businestravel i attrition
+-- businestravel->attrition
 select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by businesstravel), 2) as procent
 from (select businesstravel, attrition, count(1) as liczba_pracownikow
       from "WA_Fn-UseC_-HR-Employee-Attrition"
       group by 1, 2
       order by 1) pod;
--- department zarobki i attrition
+-- department->monthlyincome->attrition
 select *, round(liczba_pracowników / sum(liczba_pracowników) over (partition by department, zarobki), 2) as procent
 from (SELECT department,
              (case
@@ -67,13 +67,13 @@ from (SELECT department,
       group by 1, 2, 4
       order by 2, 1) pod
 order by 2, 1;
---department i attrition
+--department->attrition
 select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by department), 2) as procent
 from (select department, attrition, count(1) as liczba_pracownikow
       from "WA_Fn-UseC_-HR-Employee-Attrition"
       group by 1, 2
       order by 1) pod
--- monthlyincome, jobsatisfaction, attrition
+-- monthlyincome->jobsatisfaction->attrition
 select *, round(liczba_pracowników/sum(liczba_pracowników) over (partition by zarobki),2) as procent from (select (case
             when monthlyincome < 1000 then '<1000'
             when monthlyincome <= 3000 then '<=3000'
@@ -93,14 +93,14 @@ from "WA_Fn-UseC_-HR-Employee-Attrition"
 group by 2, 1,3
 order by 1) pod
 order by 1;
--- jobsatisfaction i attrition
+-- jobsatisfaction->attrition
 select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by jobsatisfaction), 2) as procent
 from (select jobsatisfaction, attrition, count(1) as liczba_pracownikow
       from "WA_Fn-UseC_-HR-Employee-Attrition"
       group by 1, 2
       order by 1) pod
 order by 1;
--- jobsatisfaction i monthlyincome
+-- jobsatisfaction->monthlyincome
 select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by zarobki), 2) as procent
 from (select jobsatisfaction, (case
             when monthlyincome < 1000 then '<1000'
@@ -118,3 +118,62 @@ from (select jobsatisfaction, (case
       group by 1, 2
       order by 1) pod
 order by 2;
+-- montlhyincome->jobrole->department
+select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by department), 3) as procent
+from (select jobrole, department, (case
+            when monthlyincome < 1000 then '<1000'
+            when monthlyincome <= 3000 then '<=3000'
+            when monthlyincome <= 5000 then '<=5000'
+            when monthlyincome <= 7000 then '<=7000'
+            when monthlyincome <= 9000 then '<=9000'
+            when monthlyincome <= 11000 then '<=11000'
+            when monthlyincome <= 13000 then '<=13000'
+            when monthlyincome <= 15000 then '<=15000'
+            when monthlyincome <= 17000 then '<=17000'
+            when monthlyincome <= 19000 then '<=19000'
+            when monthlyincome <= 21000 then '<=21000' end) as zarobki, count(1) as liczba_pracownikow
+      from "WA_Fn-UseC_-HR-Employee-Attrition"
+group by 1,3,2
+order by 2 desc,1) pod
+where department like 'Research & Development'
+order by 1 desc, 3 desc;
+
+select distinct department, jobrole from "WA_Fn-UseC_-HR-Employee-Attrition"
+where department like 'Research & Development';
+--Monthlyincome->EducationField->Education
+select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (), 2) as procent
+from (select Education,jobrole, count(1) as liczba_pracownikow
+      from "WA_Fn-UseC_-HR-Employee-Attrition"
+group by 1,2
+order by 2 desc,1) pod
+where jobrole like 'Research Director'
+order by 3 desc;
+
+select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by educationfield), 2) as procent
+from (select educationfield, (case
+            when monthlyincome < 1000 then '<1000'
+            when monthlyincome <= 3000 then '<=3000'
+            when monthlyincome <= 5000 then '<=5000'
+            when monthlyincome <= 7000 then '<=7000'
+            when monthlyincome <= 9000 then '<=9000'
+            when monthlyincome <= 11000 then '<=11000'
+            when monthlyincome <= 13000 then '<=13000'
+            when monthlyincome <= 15000 then '<=15000'
+            when monthlyincome <= 17000 then '<=17000'
+            when monthlyincome <= 19000 then '<=19000'
+            when monthlyincome <= 21000 then '<=21000' end) as zarobki, count(1) as liczba_pracownikow
+      from "WA_Fn-UseC_-HR-Employee-Attrition"
+group by 1,2
+order by 2 desc,1) pod
+where educationfield like 'Technical Degree'
+order by 2 desc, 3 desc;
+
+select *, round(liczba_pracownikow / sum(liczba_pracownikow) over (partition by educationfield), 3) as procent
+from (select educationfield, jobrole, count(1) as liczba_pracownikow
+      from "WA_Fn-UseC_-HR-Employee-Attrition"
+group by 1,2
+order by 2 desc,1) pod
+where educationfield like 'Technical Degree'
+order by 2 desc, 3 desc;
+
+select distinct educationfield from "WA_Fn-UseC_-HR-Employee-Attrition"
